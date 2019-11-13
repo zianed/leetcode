0981afa339ba53f2d52eb138dc09370545837b3b
zianed <zianed@live.cn>
2019-11-14 00:06:07 +0800

    2019-11-13

---
+++ cpp/547. 朋友圈.cpp
@@ -0,0 +1,120 @@
+/**
+班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+
+给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+
+示例 1:
+
+输入: 
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+输出: 2 
+说明：已知学生0和学生1互为朋友，他们在一个朋友圈。
+第2个学生自己在一个朋友圈。所以返回2。
+示例 2:
+
+输入: 
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+输出: 1
+说明：已知学生0和学生1互为朋友，学生1和学生2互为朋友，所以学生0和学生2也是朋友，所以他们三个在一个朋友圈，返回1。
+注意：
+
+N 在[1,200]的范围内。
+对于所有学生，有M[i][i] = 1。
+如果有M[i][j] = 1，则有M[j][i] = 1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/friend-circles
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+**/
+
+// 深度优先解法
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M)
+    {
+        if (M.empty()) {
+            return 0;
+        }
+        int len = M.size();
+        vector<bool> visited(len, false);
+        int num = 0;
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
+                dfs(M, len, i, visited);
+                num++;
+            }
+        }
+        return num;
+    }
+
+    void dfs(vector<vector<int>>& M, int len, int i, vector<bool> &visited)
+    {
+        visited[i] = true;
+        for (int j = 0; j < len; j++) {
+            if (M[i][j] == 1 && !visited[j]) {
+                dfs(M, len, j, visited);
+            }
+        }
+    }
+};
+
+
+// 并查集
+class DisjoinSet {
+public:
+    DisjoinSet(int num)
+        : num_(num)
+    {
+        for (int i = 0; i < num_; i++) {
+            values_.push_back(i);
+        }
+    }
+
+    void Connect(int i, int j)
+    {
+        int iRoot = FindRoot(i);
+        int jRoot = FindRoot(j);
+        if (iRoot != jRoot) {
+            values_[iRoot] = jRoot;
+            num_--;
+        }
+    }
+
+    int GetNum()
+    {
+        return num_;
+    }
+private:
+    int FindRoot(int i)
+    {
+        int root = i;
+        while (values_[root] != root) {
+            root = values_[root];
+        }
+        return root;
+    }
+
+    int num_;
+    vector<int> values_;
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M)
+    {
+        int len = M.size();
+        DisjoinSet ds(len);
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (M[i][j] == 1) {
+                    ds.Connect(i, j);
+                }
+            }
+        }
+        return ds.GetNum();
+    }
+};
